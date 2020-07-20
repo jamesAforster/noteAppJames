efa5d07542d10c831fd88f50be4cce7778894e5e
Alessa <alessa_hardwick@live.co.uk>
2020-07-20 17:34:53 +0100

    test note instance text passing

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -7,3 +7,29 @@ Expects a new instance of note which is given the argument ("This is my note")
 To respond to note.text with "This is my note"
 
 */
+
+function testNoteInstanceText() {  
+  var note = new Note("This is my note");  
+    if (note.text !== "This is my note") { 
+      console.log(`String wrong, expected "This is my note" got ${note.text}`); 
+    }
+    else {
+      console.log("Tests Passing You Rock") 
+    }  
+}; 
+testNoteInstanceText();
+
+
+// Visit homepage, expect to see text
+function testNoteTextIsOnPage(){
+  var note = new Note("This is my note");
+
+    if (!document.body.contains("This is my note")) { 
+      console.log(`no string there`); 
+    }
+    else {
+      console.log("Tests Passing You Rock") 
+    }  
+};
+
+testNoteTextIsOnPage();
