331d85397f5304ba8d851e0a569812f3d39a5dd1
James Forster <jamesalexforster@gmail.com>
2020-07-20 16:44:49 +0100

    basic file structure in place

---
+++ spec/noteSpec.js
@@ -0,0 +1,9 @@
+/*
+
+User visits homepage
+Expects to see a note
+
+Expects a new instance of note which is given the argument ("This is my note")
+To respond to note.text with "This is my note"
+
+*/
