17cf2ecce7af554baabd2fdfb0e38d9b4f1d180f
James Forster <jamesalexforster@gmail.com>
2020-07-21 16:43:10 +0100

    created controller.js and https server

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,43 +1,37 @@
-/*
-
-User visits homepage
-Expects to see a note
-
-Expects a new instance of note which is given the argument ("This is my note")
-To respond to note.text with "This is my note"
-
-*/
-
-function testNoteInstanceText() {
-  var note = new Note("This is my note");
-    if (note.text !== "This is my note") {
-      console.log(`String wrong, expected "This is my note" got ${note.text}`);
-    }
-    else {
-      console.log("Tests Passing You Rock")
-    }
-};
-testNoteInstanceText();
-
 var it = function(text, callback){
+  console.log(text);
   callback();
 };
 
 var expect = function(testedInput){
   return {
-      toEqual: console.log("getting deeper?")
-    // toEqual : function(expectedOutcome) {
-    //   if(testedInput === expectedOutcome) {
-    //     return "Tests Passing You Rock"
-    //   } else {
-    //     return "You're benched, mate."
-    //   }
-    // };
+    toEqual: function(expectedOutcome) {
+      if(testedInput === expectedOutcome) {
+        console.log("Tests Passing You Rock")
+      } else {
+        console.log("You're benched, mate.")
+      }
+    },
+
+    toNotEqual: function(expectedOutcome) {
+      if(testedInput !== expectedOutcome) {
+        console.log("Tests Passing You Rock")
+      } else {
+        console.log("You're benched, mate.")
+      }
+    },
+
   };
 };
 
-it('create text when we create an instace of note',function()
+it('Should be able to return the text that it is created with',function()
   {
     note = new Note ("This is my note")
     expect(note.text).toEqual("This is my note")
+});
+
+it('This test should pass as it is checking the given out does NOT match the actual output',function()
+  {
+    note = new Note ("This is my note")
+    expect(note.text).toNotEqual("This is not my note")
 });
