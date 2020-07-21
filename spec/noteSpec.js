d897f426cf5941982d60f324c29e221ebbf32e50
Rabee93 <rabei_mghrabi@yahoo.com>
2020-07-21 15:07:03 +0100

    added it function

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -19,17 +19,24 @@ function testNoteInstanceText() {  
 }; 
 testNoteInstanceText();
 
+var it = function(){}
+  it('creats text when we create an instace of note',function()
+  {
+    note = new Note ("This is my note")
+    expect(note.text).toEqual("This is my note")
+});
+
 
 // Visit homepage, expect to see text
-function testNoteTextIsOnPage(){
-  var note = new Note("This is my note");
-
-    if (!document.body.contains("This is my note")) { 
-      console.log(`no string there`); 
-    }
-    else {
-      console.log("Tests Passing You Rock") 
-    }  
-};
-
-testNoteTextIsOnPage();
+// function testNoteTextIsOnPage(){
+//   var note = new Note("This is my note");
+//
+//     if (!document.body.contains("This is my note")) { 
+//       console.log(`no string there`); 
+//     }
+//     else {
+//       console.log("Tests Passing You Rock") 
+//     }  
+// };
+//
+// testNoteTextIsOnPage();
