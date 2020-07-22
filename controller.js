00c02ffe78943f9aa6cdd38014767744e50cc3cb
James Forster <jamesalexforster@gmail.com>
2020-07-22 12:00:07 +0100

    we can iterate through the notes object and print them all to index

--- controller.js
+++ controller.js
@@ -1,1 +1,6 @@
-var note = new Note("hello here is a lot of lovely text")
+// var note = new Note("hello here is a lot of lovely text")
+// var note2 = new Note("hello here is a lot of lovely text")
+// var note3 = new Note("hello here is a lot of lovely text")
+// var note4 = new Note("hello here is a lot of lovely text")
+
+// var notes = [note1, note2, note3, note4]
